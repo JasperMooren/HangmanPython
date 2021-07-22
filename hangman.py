032ce4f769882891742efc60a5999efa7350f360
JasperMooren <jasper.mooren@hotmail.nl>
2021-07-22 18:39:16 +0200

    First commit of basic hangman
    Added a basic controller-model structure

---
+++ hangman.py
@@ -0,0 +1,8 @@
+def run_hangman():
+    pass
+
+
+class Hangman:
+    def __init__(self, word):
+        self.word = word
+        amount_of_mistakes = 0
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-#test test
+import hangman
+
+
+def main():
+    run_hangman()
+
+
+def run_hangman():
+    print("Welcome to Hangman!")
+    print("How many letters do you want your word to have?")
+    input1 = input()
+    # more code
+    # construct Hangman object
+    # use this object to resolve Hangman:
+    #   it can draw itself
+    #       you can show a picture of the hangman (ASCII art)
+    #       you can show the letters guessed
+    #       you can show the correct letters in the correct positions
+    #   you can ask it if you have won
+    #   you can ask it if hangman has won
+    #   you can put in letters (hangman can check this input for you)
+
+
+# Entry point of the programme
+if __name__ == '__main__':
+    main()
